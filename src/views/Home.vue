22f68d938aeefc65e364770df09a2bc32589294d
myungwonKim <k7y7mn@gmail.com>
2019-09-10 21:23:37 +0900

    Bootstrap-vue test code rollback

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,12 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--특정 테그 사용 -->
+    <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+
+    <b-modal id="modal-1" title="BootstrapVue">
+      <p class="my-4">Hello from modal!</p>
+    </b-modal>
   </div>
 </template>
 
