3e289c21a156a419d4217046bc09135d82e4efc1
myungwonKim <k7y7mn@gmail.com>
2019-09-10 21:21:36 +0900

    Bootstrap-vue test code delete

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,6 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div class="alert alert-primary" role="alert">
-      부트스트랩 되나여?
-    </div>
-
-    <!--특정 테그 사용 -->
-    <b-button v-b-modal.modal-1>Launch demo modal</b-button>
-
-    <b-modal id="modal-1" title="BootstrapVue">
-      <p class="my-4">Hello from modal!</p>
-    </b-modal>
   </div>
 </template>
 
